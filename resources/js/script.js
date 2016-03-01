6d36feb7db56aba55edcbfce92c634967b98f542
Ankita Fernandes <ankita@happydoodles.in>
2016-03-01 19:17:26 +0530

    menu click fixes

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,6 +1,21 @@
 $(document).ready(function(){
 	$(".button-collapse").sideNav();
-	//$('#menu-icon').click();
+	  
+    var isMediumAndAbove = Modernizr.mq('(min-width: 900px)')
+    if(isMediumAndAbove){    
+        $('#menu-icon').click(function(){
+            if(parseInt($('#nav-mobile').css('left')) !== 0 ){
+                $('.page-content').animate({
+                    'margin-left' : '240px'
+                }, 200);
+            }
+            else{
+                $('.page-content').animate({
+                    'margin-left' : '0px'
+                }, 200);
+            }
+        }).click();
+    }
 
 	$(".toggle-wrapper > .col").click(function(){
 		$(this).parent().find(".toggle-section").removeClass("active");
