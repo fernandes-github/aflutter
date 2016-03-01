d9320c7e5920992265d44b3891e78425d460d259
Ankita Fernandes <fernandesankita@outlook.com>
2016-03-01 18:47:16 +0530

    added script for toggle and fonts

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,4 +1,9 @@
 $(document).ready(function(){
 	$(".button-collapse").sideNav();
 	//$('#menu-icon').click();
+
+	$(".toggle-wrapper > .col").click(function(){
+		$(this).parent().find(".toggle-section").removeClass("active");
+		$(this).find(".toggle-section").addClass("active");
+	})
 })
