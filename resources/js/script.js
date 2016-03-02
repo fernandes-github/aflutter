97b639056fd4a06697bb070e5c549fcc6782e613
Ankita Fernandes <fernandesankita@outlook.com>
2016-03-02 16:39:07 +0530

    resolved the issues of responsive and css and added range-slider and started with new page people-collapsed

--- resources/js/script.js
+++ resources/js/script.js
@@ -21,4 +21,26 @@ $(document).ready(function(){
 		$(this).parent().find(".toggle-section").removeClass("active");
 		$(this).find(".toggle-section").addClass("active");
 	})
+
+    $(function() {
+        $( "#slider-range" ).slider({
+          range: true,
+          min: 0,
+          max: 500,
+          values: [ 75, 300 ],
+          slide: function( event, ui ) {
+            $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+          }
+        });
+        $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
+          " - $" + $( "#slider-range" ).slider( "values", 1 ) );
+      })
+
+
+    $("#slider").dateRangeSlider()
+
+
+    $(document).ready(function(){
+      $('.modal-trigger').leanModal();
+    })
 })
