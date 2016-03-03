182a00da2c70144820d2ee2c97bc8cfcf42f5bcc
Ankita Fernandes <fernandesankita@outlook.com>
2016-03-03 19:51:32 +0530

    added to-do modal and css for both

--- resources/js/script.js
+++ resources/js/script.js
@@ -43,4 +43,15 @@ $(document).ready(function(){
       }, 'slow');
     });
 
+    $(function(){
+      $(".modal-content").slimScroll({
+          height: '550px'
+      });
+  });
+
+    $(".datepicker").pickadate({
+      selectMonths: true, // Creates a dropdown to control month
+      selectYears: 15 // Creates a dropdown of 15 years to control year
+    });
+
 });
