0e21c8129a143b66d4db6442a0ec35f9901079de
Ankita Fernandes <ankita@happydoodles.in>
2016-03-03 10:37:04 +0530

    js fixes

--- resources/js/script.js
+++ resources/js/script.js
@@ -17,30 +17,30 @@ $(document).ready(function(){
         }).click();
     }
 
-	$(".toggle-wrapper > .col").click(function(){
-		$(this).parent().find(".toggle-section").removeClass("active");
-		$(this).find(".toggle-section").addClass("active");
-	})
+  	$(".toggle-wrapper > .col").click(function(){
+  		$(this).parent().find(".toggle-section").removeClass("active");
+  		$(this).find(".toggle-section").addClass("active");
+  	});
 
-    $(function() {
-        $( "#slider-range" ).slider({
-          range: true,
-          min: 0,
-          max: 500,
-          values: [ 75, 300 ],
-          slide: function( event, ui ) {
-            $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
-          }
-        });
-        $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
-          " - $" + $( "#slider-range" ).slider( "values", 1 ) );
-      })
+    setTimeout(function(){
+      $("#slider").dateRangeSlider();
+    },300);
 
 
-    $("#slider").dateRangeSlider()
+    $('.modal-trigger').leanModal();
 
+    $('#nav-mobile .group a').click(function(e){
+      e.stopPropagation();
+      e.preventDefault();
+      var hash = $(this).attr('href');
+      $('#nav-mobile .group').removeClass('active');
+      $('.people-collapsed-wrapper h4').removeClass('active');
+      $(this).parent().addClass('active');
+      $(hash).find('h4').addClass('active');
+      var scrollOffset = $('#couple').parent().offset().top + 50;
+      $('html,body').animate({
+        scrollTop : $(hash).offset().top - scrollOffset
+      }, 'slow');
+    });
 
-    $(document).ready(function(){
-      $('.modal-trigger').leanModal();
-    })
-})
+});
