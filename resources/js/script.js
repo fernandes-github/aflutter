825aecfe969e59708aefc7c1b6f37d64757d2463
Ankita Fernandes <fernandesankita@outlook.com>
2016-03-28 12:24:59 +0530

    added the fullcalendar plugin

--- resources/js/script.js
+++ resources/js/script.js
@@ -412,4 +412,55 @@ $(document).ready(function(){
 
       //////////////////////////////////////////////////////////////////////////
 
+      setTimeout(function(){
+        $('#calendar').fullCalendar({
+          header: {
+            left: '',
+            center: '',
+            right: ''
+          },
+          defaultDate: '2014-06-12',
+          defaultView: 'agendaDay',
+          editable: true,
+          events: [
+            {
+              title: 'All Day Event',
+              start: '2014-06-01'
+            },
+            {
+              title: 'Long Event',
+              start: '2014-06-07',
+              end: '2014-06-10'
+            },
+            {
+              id: 999,
+              title: 'Repeating Event',
+              start: '2014-06-09T16:00:00'
+            },
+            {
+              id: 999,
+              title: 'Repeating Event',
+              start: '2014-06-16T16:00:00'
+            },
+            {
+              title: 'Meeting',
+              start: '2014-06-12T00:30:00',
+              end: '2014-06-12T00:30:00'
+            },
+            {
+              title: 'Lunch',
+              start: '2014-06-12T12:00:00'
+            },
+            {
+              title: 'Birthday Party',
+              start: '2014-06-13T07:00:00'
+            },
+            {
+              title: 'Click for Google',
+              url: 'http://google.com/',
+              start: '2014-06-28'
+            }
+          ]
+        });
+      }, 30);
 });
